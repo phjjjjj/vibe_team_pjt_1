f868245c72374e2da49d08d5a2c93cc48cce87e5
hanseongyeon <han.seongyeon@gmail.com>
2026-07-15 10:49:12 +0900

    calendar try

--- my-app/src/components/CalendarPage.vue
+++ my-app/src/components/CalendarPage.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    <h2>캘린더</h2>
+  <div class="post-create-page">
+    <!-- 기존 폼 내용 -->
+    ...
+    <!-- 임시로 캘린더 삽입 -->
+    <hr />
+    <h3>캘린더 (테스트)</h3>
     <Calendar />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useBoardStore } from '../store'
 import Calendar from '../components/Calendar.vue'
 </script>
 
