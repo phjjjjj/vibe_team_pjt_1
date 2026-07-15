75b03993fba4af3227a70ce845606d949247e660
berryjiny <berryjiny@naver.com>
2026-07-15 09:18:23 +0900

    router2

---
+++ my-app/src/board/BoardPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>게시판 목록</h2>
+    <p>게시판 페이지가 정상적으로 로드되었습니다.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ my-app/src/post/PostCreatePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>글 작성</h2>
+    <p>여기에 글 작성 폼을 넣을 수 있습니다.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
