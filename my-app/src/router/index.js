23ddcacb9f01f30eb79da39ab3a5e2fc021fb98c
scottyj0729 <scottyj0729@gmail.com>
2026-07-15 13:45:49 +0900

    editPost

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -3,11 +3,13 @@ import HomePage from '../HomePage.vue'
 import BoardPage from '../board/BoardPage.vue'
 import PostCreatePage from '../post/PostCreatePage.vue'
 import CalendarPage from '../components/CalendarPage.vue'
+import PostEditPage from '../post/PostEditPage.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/board', name: 'Board', component: BoardPage },
   { path: '/board/write', name: 'PostCreate', component: PostCreatePage },
+  { path: '/board/:id/edit', name: 'PostEdit', component: PostEditPage },
   { path: '/calendar', name: 'Calendar', component: CalendarPage },
 ]
 
